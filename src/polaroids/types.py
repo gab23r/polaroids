9d8aee956b5ed9ca6870a2916f97c6e962ba0fe3
Cangyuan Li <everest229@gmail.com>
2025-04-18 15:02:09 -0700

    feat: use NewType instead of type to support python < 3.12

--- src/polaroids/types.py
+++ src/polaroids/types.py
@@ -1,15 +1,15 @@
 """Type aliases module."""
 
-type uint8 = int
-type uint16 = int
-type uint32 = int
-type uint64 = int
+from typing import NewType
 
-type int8 = int
-type int16 = int
-type int32 = int
-type int64 = int
-type int128 = int
-
-type float32 = float
-type float64 = float
+uint8 = NewType("uint8", int)
+uint16 = NewType("uint16", int)
+uint32 = NewType("uint32", int)
+uint64 = NewType("uint64", int)
+int8 = NewType("int8", int)
+int16 = NewType("int16", int)
+int32 = NewType("int32", int)
+int64 = NewType("int64", int)
+int128 = NewType("int128", int)
+float32 = NewType("float32", float)
+float64 = NewType("float64", float)
